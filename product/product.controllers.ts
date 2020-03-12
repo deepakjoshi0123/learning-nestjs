9261dafd8663ceeb187c509084c70d9d1ac601fd
Deepak joshi <deepakjoshi0123@gmail.com>
2020-03-11 23:30:07 -0700

    product apis working fine

--- product/product.controllers.ts
+++ product/product.controllers.ts
@@ -3,19 +3,17 @@ import {
   Post,
   Get,
   Param,
-  Patch,
   Delete,
   Body,
+  Query,
 } from '@nestjs/common';
-
 import { ProductsService } from './product.service';
-import { ProductModule } from './product.module';
 
-@Controller('products')
+@Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
+  @Post('addproduct')
   addProduct(
     @Body('ProductName') ProductName: string,
     @Body('ProductType') ProductType: string,
@@ -27,5 +25,28 @@ export class ProductsController {
       Price,
     );
     return { id: generatedId };
+  }
+  @Get('getproducts')
+  getproducts(@Query('start') start1: number, @Query('end') end1: number) {
+    return this.productsService.getProducts(start1, end1);
+  }
+  @Post('edit/:id')
+  editproduct(
+    @Param('id') id: number,
+    @Body('ProductName') ProductName: string,
+    @Body('ProductType') ProductType: string,
+    @Body('Price') Price: string,
+  ) {
+    this.productsService.postEditProduct(id, ProductName, ProductType, Price);
+    return 'updated succesfully';
+  }
+  @Delete('delete/:id')
+  delproduct(@Param('id') id: number) {
+    this.productsService.deleteProduct(id);
+    return 'deleted succesfully';
+  }
+  @Get('search/:ProductName')
+  searchProduct(@Param('ProductName') ProductName: string) {
+    return this.productsService.searchProduct(ProductName);
   }
 }
--- product/product.service.ts
+++ product/product.service.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-  Inject,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Product } from '../product/product.entity';
 
 @Injectable()
@@ -20,12 +15,74 @@ export class ProductsService {
         Price: Price,
       });
       if (!result) throw new Error('unable to insert');
+      return result.id;
     } catch (error) {
       console.log(error);
     }
   }
-  getProducts() {}
-  //   postEditProduct() {}
-  //   deleteProduct() {}
-  //   searchProduct() {}
+  async getProducts(start: number, end: number) {
+    console.log('helloo ', start, end);
+    try {
+      const products = await Product.findAll({
+        offset: start,
+        limit: end,
+      });
+      if (!products) {
+        const error = new Error('unable to get products');
+        throw error;
+      }
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async postEditProduct(
+    id: number,
+    ProductName: string,
+    ProductType: string,
+    Price: string,
+  ) {
+    try {
+      const product = await Product.findByPk(id);
+      product.ProductName = ProductName;
+      product.ProductType = ProductType;
+      product.Price = Price;
+      const resSave = await product.save();
+
+      if (!resSave || !product) {
+        const err = new Error('error in udpatation');
+        throw err;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async deleteProduct(id: number) {
+    try {
+      const result = await Product.destroy({
+        where: { id: id },
+      });
+      if (!result) {
+        const err = new Error('error in deletion');
+        throw err;
+      }
+      return 'deleted successfully';
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async searchProduct(ProductName: string) {
+    try {
+      const result = await Product.findAll({
+        where: { ProductName: ProductName },
+      });
+      if (!result) {
+        const err = new Error('error in search');
+        throw err;
+      }
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
